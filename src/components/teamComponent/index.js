2bbcd2ee3f586b406fc4704b347f4879724a23f8
PabloMEcheverria <pablomartinecheverria@gmail.com>
2023-07-13 01:41:07 -0300

    Deleting unused imports in teamComponent.

--- src/components/teamComponent/index.js
+++ src/components/teamComponent/index.js
@@ -2,9 +2,6 @@ import "./teamComponent.css"
 import "../../App.css"
 import Collaborator from "../collaborator";
 import hexToRgba from 'hex-to-rgba';
-//import rgbToHex from 'rgba-to-hex-colors';
-//const { rgbToHex } = require('rgba-to-hex-colors');
-import rgbHex from 'rgb-hex';
 
 export default function TeamComponent (props) {
     const {title, id, primaryColor/*, secondaryColor*/} = props.data;
